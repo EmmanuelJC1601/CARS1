126f9a7eeaab90ac208a210ce3caba0efcc91d8d
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-09-22 21:44:15 -0600

    Update Pilotos.java

--- Pilotos.java
+++ Pilotos.java
@@ -5,7 +5,23 @@ public class Pilotos {
     private String escuderia;
     private int no_carro;
 
-    public Pilotos(){
+    public Pilotos(String nombre, String nacionalidad, String escuderia, int no_carro){
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.escuderia = escuderia;
+        this.no_carro = no_carro;
+    }
 
+    public String getNombre(){
+        return nombre;
+    }
+    public String getNacionalidad(){
+        return nacionalidad;
+    }
+    public String getEscuderia(){
+        return escuderia;
+    }
+    public int getNo_Carro(){
+        return no_carro;
     }
 }
