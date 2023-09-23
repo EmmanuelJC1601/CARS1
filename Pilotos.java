8a4e67bad2a1767f4b81bdadaaf92734a29fbda9
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-09-22 21:38:00 -0600

    Create Pilotos.java

---
+++ Pilotos.java
@@ -0,0 +1,11 @@
+public class Pilotos {
+    
+    private String nombre;
+    private String nacionalidad;
+    private String escuderia;
+    private int no_carro;
+
+    public Pilotos(){
+
+    }
+}
