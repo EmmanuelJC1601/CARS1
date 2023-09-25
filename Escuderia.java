eefd038c47fb0ef74d2f09be9b57d631cb774ee2
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-09-24 20:53:12 -0600

    Create Escuderia.java

---
+++ Escuderia.java
@@ -0,0 +1,24 @@
+public class Escuderia {
+    
+    private String Marca;
+    private Pilotos piloto_1;
+    private Pilotos piloto_2;
+    static int contador_Escu = 0;
+
+    public Escuderia(String Marca){
+        this.Marca = Marca;
+        contador_Escu += 1;
+    }
+    public String getMarca(){
+        return Marca;
+    }
+    public Pilotos getPiloto_1(){
+        return piloto_1;
+    }
+    public Pilotos getPiloto_2(){
+        return piloto_2;
+    }
+    public void Imprimir(){
+        System.out.println("- - - - - "+getMarca()+" - - - - -");
+    }
+}
