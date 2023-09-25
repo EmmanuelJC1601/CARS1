50eebbe7040b0bd74ccd3b934a6f02ffff260db0
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-09-24 20:53:20 -0600

    Create Campeonato.java

---
+++ Campeonato.java
@@ -0,0 +1,10 @@
+public class Campeonato {
+    
+    private String nombre;
+    private String pais;
+    private int año;
+
+    Carrera[] carreras = new Carrera[22];
+    static int cont_carreras = 0;
+
+}
