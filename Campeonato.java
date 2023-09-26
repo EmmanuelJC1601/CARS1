70a9d95bcc8ba5a1eba3a762fa0b4118f088b932
Eduardoavenda <avendanocalderonemv3@gmail.com>
2023-09-26 11:50:42 -0600

    Update Campeonato.java

--- Campeonato.java
+++ Campeonato.java
@@ -1,10 +1,8 @@
 public class Campeonato {
-    
+   
     private String nombre;
-    private String pais;
     private int año;
 
     Carrera[] carreras = new Carrera[23];
     static int cont_carreras = 0;
-
 }
