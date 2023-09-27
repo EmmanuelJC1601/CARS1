d89eac524d08b34d08586690488d14ab8f9bb30c
Eduardoavenda <avendanocalderonemv3@gmail.com>
2023-09-27 09:29:14 -0600

    Update Pilotos.java

--- Pilotos.java
+++ Pilotos.java
@@ -2,26 +2,25 @@ public class Pilotos {
     
     private String nombre;
     private String nacionalidad;
-    private String escuderia;
     private int no_carro;
 
-    public Pilotos(String nombre, String nacionalidad, String escuderia, int no_carro){
+    public Pilotos(String nombre, String nacionalidad, int no_carro){
         this.nombre = nombre;
         this.nacionalidad = nacionalidad;
-        this.escuderia = escuderia;
         this.no_carro = no_carro;
     }
-
     public String getNombre(){
         return nombre;
     }
     public String getNacionalidad(){
         return nacionalidad;
     }
-    public String getEscuderia(){
-        return escuderia;
-    }
     public int getNo_Carro(){
         return no_carro;
+    }
+    public void Imprimir(){
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Nacionalidad: " + getNacionalidad());
+        System.out.println("Número de carro: " + getNo_Carro()); 
     }
 }
