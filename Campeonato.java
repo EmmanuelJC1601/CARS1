b2ab254dfce6b039848f16d0c62be9f62a4c610d
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-09-27 10:37:56 -0600

    Update Campeonato.java

--- Campeonato.java
+++ Campeonato.java
@@ -1,8 +1,21 @@
+import java.util.*;
+
 public class Campeonato {
    
     private String nombre;
     private int año;
-
     Carrera[] carreras = new Carrera[23];
-    static int cont_carreras = 0;
+    Escuderia[] escuderias = new Escuderia[10];
+    //Pilotos[] pilotos = new Pilotos[20];
+    // Lista de Fechas
+    public Campeonato(String nombre, int año){
+        this.nombre = nombre;
+        this.año = año;
+    }
+    public void RegistrarEscuderia(String nombreEscuderia){
+        Escuderia patron = new Escuderia(nombreEscuderia);
+        
+        escuderias[patron.contador_Escu-1] = 0;
+    }
+
 }
