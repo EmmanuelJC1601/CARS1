a0f8d271db0bc4f2c061be76cbc2ec77e61d132f
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-09-27 09:37:20 -0600

    Update Escuderia.java

--- Escuderia.java
+++ Escuderia.java
@@ -12,15 +12,10 @@ public class Escuderia {
     public String getMarca(){
         return Marca;
     }
-    public Pilotos getPiloto_1(){
-        return piloto_1;
-    }
-    public Pilotos getPiloto_2(){
-        return piloto_2;
-    }
-    public void Imprimir(){
+
+    /*public void Imprimir(){
         System.out.println("- - - - - - "+getMarca()+" - - - - - -");
         System.out.println("Piloto: "+getPiloto_1());
         System.out.println("Piloto: "+getPiloto_2());
-    }
+    }*/
 }
