eac4e1d2f3dc61b35c14964c024fd3f6f9eada2c
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-09-27 09:37:16 -0600

    Update Cars4.java

--- Cars4.java
+++ Cars4.java
@@ -2,5 +2,49 @@ import java.util.*;
 
 public class Cars4 {
     
+    public static void main(String[] args) {
+        int op = 0; // Variable para las opciones
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n------------------------------ Formula 1 -------------------------\n");
+
+        do {
+            System.out.println("--------------- Menu de Opciones ---------------\n");
+            System.out.print("1. Crear Campeonato\n2. Iniciar Carrera\n");
+            System.out.print("3. Consultar Carreras Anteriores\n4. Finalizar Campeonato\n");
+            System.out.print("5. Mostrar Elementos\n6. Cerrar Programa\n");
+            System.out.println("Opcion: ");
+            op = sc.nextInt();
+
+            switch (op) {
+                case 1:
+                    
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    return;
+
+                default:
+                    System.out.println("!!! ERROR !!! Opcion NO Disponible");
+                    break;
+            }
+
+
+
+
+
+
+        } while (op != 6);
+    }
 
 }
