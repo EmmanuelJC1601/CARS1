b75cc2766fc1c712e5141d1dd35ab18248acbb17
EmmanuelJC1601 <emmanueljc1601@gmail.com>
2023-09-27 22:56:11 -0600

    Update Campeonato.java

--- Campeonato.java
+++ Campeonato.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Campeonato {
    
     private String nombre;
-    private int año;
+    private int year;
     Carrera[] carreras = new Carrera[23];
     Escuderia[] escuderias = new Escuderia[10];
     //Pilotos[] pilotos = new Pilotos[20];
     // Lista de Fechas
-    public Campeonato(String nombre, int año){
+    public Campeonato(String nombre, int year){
         this.nombre = nombre;
-        this.año = año;
+        this.year = year;
     }
     public void RegistrarEscuderia(String nombreEscuderia){
         Escuderia patron = new Escuderia(nombreEscuderia);
